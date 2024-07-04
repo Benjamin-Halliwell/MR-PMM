9ea7c8d6dfbcc492b10edde61700836b419158d2
Beggle <75349776+Beggle@users.noreply.github.com>
2024-07-04 17:46:04 +1000

    changes

--- MR-PMM_euc_example_functions.R
+++ MR-PMM_euc_example_functions.R
@@ -39,3 +39,86 @@ wtd.stderror <- function(x, w){
 }
 
 se <- function(x){sd(x)/sqrt(length(x))}
+
+tree.ultra <- function(tree, data) {
+  phy <- keep.tip(tree, data[data$taxon%in%tree$tip.label,]$taxon)
+  for (i in 1:3){phy$edge.length[phy$edge.length<=0.00001] <- 0.005
+  phy <- multi2di(phy)
+  phy <- force.ultrametric(phy, method = "extend", message=F)}
+  phy$node.label <- (length(phy$tip.label)+1):(phy$Nnode+length(phy$tip.label))
+  return(phy)
+}
+
+level_cor_vcv <- function(vcv, n_resp, part.1 = "phylo", part.2 =  "taxon", lambda = NULL) {
+  
+  # index corrmat elements for output
+  len_cor <- choose(n_resp,2)
+  m <- matrix(1:(n_resp^2),n_resp,n_resp)
+  for (i in 1:nrow(m)) {
+    for (j in i:(ncol(m))) {
+      m[i,j] <- 0
+    }
+  }
+  cols <- unique(as.vector(m));cols <- cols[cols!=0]
+  
+  # phylogenetic level
+  draws <- vcv %>% as_tibble() %>% rename_with(tolower) %>% rename_with(~str_remove_all(., 'trait'))
+  cor_draws <- draws %>% dplyr::select(all_of(contains(part.1))) %>% rename_with(~str_remove_all(., paste0('.',part.1)))
+  phy_cor <- matrix(nrow = nrow(cor_draws), ncol = ncol(cor_draws))
+  par_phy_cor <- matrix(nrow = nrow(cor_draws), ncol = ncol(cor_draws))
+  shrink_par_phy_cor <- matrix(nrow = nrow(cor_draws), ncol = ncol(cor_draws))
+  for (i in 1:nrow(cor_draws)){
+    cor <- unlist(cor_draws[i,])
+    phy_res <- matrix(0, n_resp, n_resp)
+    phy_res[] <- cor
+    phy_res <- cov2cor(phy_res)
+    par_phy_res <- corpcor::cor2pcor(phy_res) # compute partial correlation
+    shrink_par_phy_res <- if(is.null(lambda)) corpcor::pcor.shrink(phy_res, verbose = F)
+    else(corpcor::pcor.shrink(phy_res, lambda = lambda, verbose = F))# compute shrinkage estimates of partial correlation
+    phy_cor[i,] <- as.vector(phy_res)
+    par_phy_cor[i,] <- as.vector(par_phy_res)
+    shrink_par_phy_cor[i,] <- as.vector(shrink_par_phy_res)
+  }
+  phy_cor <- phy_cor %>% as_tibble %>% select(all_of(cols)) %>% setNames(names(cor_draws %>% select(all_of(cols)))) # %>% mcmc_intervals(prob_outer = 0.95)
+  par_phy_cor <- par_phy_cor %>% as_tibble %>% select(all_of(cols)) %>% setNames(names(cor_draws %>% select(all_of(cols)))) # %>% mcmc_intervals(prob_outer = 0.95)
+  shrink_par_phy_cor <- shrink_par_phy_cor %>% as_tibble %>% select(all_of(cols)) %>% setNames(names(cor_draws %>% select(all_of(cols)))) # %>% mcmc_intervals(prob_outer = 0.95)
+  phy_res[lower.tri(phy_res)] <- phy_cor %>% summarise(across(everything(), ~ mean(.))) %>% slice(1) %>% as.numeric(); phy_res[upper.tri(phy_res)] <- t(phy_res)[upper.tri(phy_res)]; dimnames(phy_res)[[1]] <- gsub(":","",gsub("trait","",gsub("[^:]+$","",colnames(vcv)[1:n_resp])));dimnames(phy_res)[[2]] <- gsub(":","",gsub("trait","",gsub("[^:]+$","",colnames(vcv)[1:n_resp])))
+  par_phy_res[lower.tri(par_phy_res)] <- par_phy_cor %>% summarise(across(everything(), ~ mean(.))) %>% slice(1) %>% as.numeric(); par_phy_res[upper.tri(par_phy_res)] <- t(par_phy_res)[upper.tri(par_phy_res)]; dimnames(par_phy_res)[[1]] <- gsub(":","",gsub("trait","",gsub("[^:]+$","",colnames(vcv)[1:n_resp])));dimnames(par_phy_res)[[2]] <- gsub(":","",gsub("trait","",gsub("[^:]+$","",colnames(vcv)[1:n_resp])))
+  shrink_par_phy_res[lower.tri(shrink_par_phy_res)] <- shrink_par_phy_cor %>% summarise(across(everything(), ~ mean(.))) %>% slice(1) %>% as.numeric(); shrink_par_phy_res[upper.tri(shrink_par_phy_res)] <- t(shrink_par_phy_res)[upper.tri(shrink_par_phy_res)]; dimnames(shrink_par_phy_res)[[1]] <- gsub(":","",gsub("trait","",gsub("[^:]+$","",colnames(vcv)[1:n_resp])));dimnames(shrink_par_phy_res)[[2]] <- gsub(":","",gsub("trait","",gsub("[^:]+$","",colnames(vcv)[1:n_resp])))
+  
+  
+  
+  # residual level
+  draws <- vcv %>% as_tibble() %>% rename_with(tolower) %>% rename_with(~str_remove_all(., 'trait'))
+  cor_draws <- draws %>% dplyr::select(all_of(contains(part.2))) %>% rename_with(~str_remove_all(., paste0('.',part.2)))
+  ind_cor <- matrix(nrow = nrow(cor_draws), ncol = ncol(cor_draws))
+  par_ind_cor <- matrix(nrow = nrow(cor_draws), ncol = ncol(cor_draws))
+  shrink_par_ind_cor <- matrix(nrow = nrow(cor_draws), ncol = ncol(cor_draws))
+  for (i in 1:nrow(cor_draws)){
+    cor <- unlist(cor_draws[i,])
+    ind_res <- matrix(0, n_resp, n_resp)
+    ind_res[] <- cor
+    ind_res <- cov2cor(ind_res)
+    par_ind_res <- corpcor::cor2pcor(ind_res) # compute partial correlation
+    shrink_par_ind_res <- if(is.null(lambda)) corpcor::pcor.shrink(ind_res, verbose = F)
+    else(corpcor::pcor.shrink(ind_res, lambda = lambda, verbose = F)) # compute shrinkage estimates of partial correlation
+    ind_cor[i,] <- as.vector(ind_res)
+    par_ind_cor[i,] <- as.vector(par_ind_res)
+    shrink_par_ind_cor[i,] <- as.vector(shrink_par_ind_res)
+  }
+  ind_cor <- ind_cor %>% as_tibble %>% select(all_of(cols)) %>% setNames(names(cor_draws %>% select(all_of(cols)))) # %>% mcmc_intervals(prob_outer = 0.95)
+  par_ind_cor <- par_ind_cor %>% as_tibble %>% select(all_of(cols)) %>% setNames(names(cor_draws %>% select(all_of(cols)))) # %>% mcmc_intervals(prob_outer = 0.95)
+  shrink_par_ind_cor <- shrink_par_ind_cor %>% as_tibble %>% select(all_of(cols)) %>% setNames(names(cor_draws %>% select(all_of(cols)))) # %>% mcmc_intervals(prob_outer = 0.95)
+  ind_res[lower.tri(ind_res)] <- ind_cor %>% summarise(across(everything(), ~ mean(.))) %>% slice(1) %>% as.numeric(); ind_res[upper.tri(ind_res)] <- t(ind_res)[upper.tri(ind_res)]; dimnames(ind_res)[[1]] <- gsub(":","",gsub("trait","",gsub("[^:]+$","",colnames(vcv)[1:n_resp])));dimnames(ind_res)[[2]] <- gsub(":","",gsub("trait","",gsub("[^:]+$","",colnames(vcv)[1:n_resp])))
+  par_ind_res[lower.tri(par_ind_res)] <- par_ind_cor %>% summarise(across(everything(), ~ mean(.))) %>% slice(1) %>% as.numeric(); par_ind_res[upper.tri(par_ind_res)] <- t(par_ind_res)[upper.tri(par_ind_res)]; dimnames(par_ind_res)[[1]] <- gsub(":","",gsub("trait","",gsub("[^:]+$","",colnames(vcv)[1:n_resp])));dimnames(par_ind_res)[[2]] <- gsub(":","",gsub("trait","",gsub("[^:]+$","",colnames(vcv)[1:n_resp])))
+  shrink_par_ind_res[lower.tri(shrink_par_ind_res)] <- shrink_par_ind_cor %>% summarise(across(everything(), ~ mean(.))) %>% slice(1) %>% as.numeric(); shrink_par_ind_res[upper.tri(shrink_par_ind_res)] <- t(shrink_par_ind_res)[upper.tri(shrink_par_ind_res)]; dimnames(shrink_par_ind_res)[[1]] <- gsub(":","",gsub("trait","",gsub("[^:]+$","",colnames(vcv)[1:n_resp])));dimnames(shrink_par_ind_res)[[2]] <- gsub(":","",gsub("trait","",gsub("[^:]+$","",colnames(vcv)[1:n_resp])))
+  
+  
+  result <- list(posteriors = list(phy_cor=phy_cor,par_phy_cor=par_phy_cor,shrink_par_phy_cor=shrink_par_phy_cor,
+                                   ind_cor=ind_cor,par_ind_cor=par_ind_cor,shrink_par_ind_cor=shrink_par_ind_cor), 
+                 matrices = list(phy_mat = phy_res, par_phy_mat = par_phy_res, shrink_par_phy_mat = shrink_par_phy_res,
+                                 ind_mat = ind_res, par_ind_mat = par_ind_res, shrink_par_ind_mat = shrink_par_ind_res))
+  
+  return(result)
+  
+}
